c0b5c98404204a1582d078b0f9d0893a8017c841
Frederico Falcao <fmf@afm.co.ao>
2021-05-15 11:38:41 +0100

    new code

---
+++ Table.php
@@ -0,0 +1,30 @@
+<?php
+
+class Table {
+
+	private $data;
+	private $headerCols;
+
+	public function addAssocArray(array $data) {
+		$this->headerCols = array_keys($data);
+		$this->data = $data; 
+		return $this;
+	}
+
+	public function render() {
+		return (new DOMEl("table"))
+			->addClass("table")
+			->addChild( (new DOMEl("thead"))
+				->addChild( (new DOMEl("tr"))
+					->addChildren( array_map(function($o) {return (new DOMEl("th"))->setText($o);},$this->header) )
+				)
+			)
+			->addChild( (new DOMEl("tbody"))
+				->addChild( (new DOMEl("tr"))
+					->addChildren( array_map(function($o) {return (new DOMEl("td"))->setText($o);},$this->header) )
+				)
+			)
+		;
+	}
+
+}
