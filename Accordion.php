355c27687f40a90d8246b955b74a4409637e3181
Frederico Falcao <fmf@afm.co.ao>
2021-05-15 17:12:41 +0100

    new code

---
+++ Accordion.php
@@ -0,0 +1,43 @@
+<?php
+
+class Accordion {
+	
+	private $items = [];
+
+	public function addAccordionItems(array $items) { foreach($items as $item) call_user_func_array([$this,"addAccordionItem"],$item); return $this; }
+	public function addAccordionItem(string $label, DOMEl $content)  { $this->items[] = ["label"=>$label, "content" => $content]; return $this;}
+	public function asDOMEl() {
+		$md5OfWholeAccordion = md5(json_encode($this->items));
+		return (new DOMEl("div"))
+			->addClass("accordion")
+			->attr("id","_".$md5OfWholeAccordion)
+			->addChildren(
+				array_map(function($item) { 
+					extract($item);
+					return (new DOMEl("div"))
+					->addClass("accordion-item")
+					->addChild(
+						(new DOMEl("h2"))
+						->addClass("accordion-header")
+						->addChild( (new DOMEl("button"))
+								->addClass("accordion-button")
+								->attr("type","button")
+								->attr("data-bs-toggle","collapse")
+								->attr("data-bs-target","#_".md5($content))
+								->setText($label)
+						)
+					)->addChild(
+						(new DOMEl("div"))
+						->addClass(["accordion-collapse","collapse","show"])
+						->attr("data-bs-parent","#_".$md5OfWholeAccordion)
+						->addChild(
+							( new DOMEl("div") )
+							->addClass("accordion-body")
+							->addChild( $content )
+				
+						)
+					);
+			     },$this->items)
+			);
+	}
+}
